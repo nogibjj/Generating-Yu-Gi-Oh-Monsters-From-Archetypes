1db39b953eb76fa1ac74e9729fb37cfda587eb21
Eric Ríos <70504872+EricR401S@users.noreply.github.com>
2024-04-29 10:48:26 +0000

    impossible

---
+++ test_scraper.py
@@ -0,0 +1,7 @@
+from pipeline import scrape_archetypes
+
+
+
+if __name__ == "__main__":
+    archetypes = ["Dark Magician", "NEos", '     Blue-Eyes']
+    scrape_archetypes(archetypes, data_path="training_images")
