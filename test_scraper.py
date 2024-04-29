59bf0aa8e0c099a51fd0d644446a66151d29caa3
Eric Ríos <70504872+EricR401S@users.noreply.github.com>
2024-04-29 10:51:30 +0000

    impossible all here

--- test_scraper.py
+++ test_scraper.py
@@ -3,5 +3,5 @@ from pipeline import scrape_archetypes
 
 
 if __name__ == "__main__":
-    archetypes = ["Dark Magician", "NEos", '     Blue-Eyes']
+    archetypes = "Dark Magician,NEos, Blue-Eyes"
     scrape_archetypes(archetypes, data_path="training_images")
