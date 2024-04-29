70bc11d66623c14f84ba459edc31a46bca3a6f41
Eric Ríos <70504872+EricR401S@users.noreply.github.com>
2024-04-29 03:35:12 +0000

    tracking app template

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-# from PIL import Image
+from PIL import Image
 # import requests
 
 # Function to generate random text
@@ -16,7 +16,39 @@ def generate_image():
 
 # Main function to run the Streamlit app
 def main():
-    st.title("Generate Random Content")
+
+    st.set_page_config(
+    page_title="Yu-Gi-Oh! Card Generator",
+    page_icon="https://upload.wikimedia.org/wikipedia/en/2/2b/Yugioh_Card_Back.jpg",
+    layout="centered",
+    initial_sidebar_state="expanded",
+    menu_items={
+    'Get help': 'https://github.com/nogibjj/Generating-Yu-Gi-Oh-Monsters-From-Archetypes/',
+    'Report a bug': "https://github.com/nogibjj/Generating-Yu-Gi-Oh-Monsters-From-Archetypes/",
+    'About': "# Made by Eric Rios and Shin Jiwon. This is an *extremely* cool app! Our repository is https://github.com/nogibjj/Generating-Yu-Gi-Oh-Monsters-From-Archetypes/"
+    }
+    )
+
+    image = Image.open("logo.jpg")
+    resized_image = image.resize((500, 500))
+    left_co, cent_co,last_co = st.columns(3)
+    with cent_co:
+        st.image(image)  # Set the width to the desired value
+
+    st.title("Welcome to the Yu-Gi-Oh! Card Archetype Generator")
+    st.markdown(
+        """
+        Welcome to the Yu-Gi-Oh! Card Archetype Generator. \n
+        Here you can generate cards by providing the keywords of the archetype that you are interested in generating. 
+        Provide the keyword(s), separated by commas, and press "get archetype". \n
+
+        Example Inputs : "Blue-Eyes" or "Dark Magician, Swordswoman" \n
+        """
+    )
+        
+    # st.title("Generate Random Content")
+
+
 
     # Button to generate random text
     if st.button("Generate Text"):
