9c9758ac1d9587e0bbd76c69a58e151d4ddeffcf
Eric Ríos <70504872+EricR401S@users.noreply.github.com>
2024-05-01 06:40:17 +0000

    experimentnts

--- magician_scraper.py
+++ magician_scraper.py
@@ -3,7 +3,7 @@ from pipeline import scrape_archetypes
 
 
 if __name__ == "__main__":
-    archetypes = "Dark Magician"
+    archetypes = "Magician"
     scrape_archetypes(archetypes, data_path="GAN_experiments/training_magician/darkmagician", 
-                      csv_path="GAN_experiments", csv_name="training_cards.csv",
-                      card_types = ["monster"])
+                      csv_path="GAN_experiments", csv_name="training_cards.csv")
+                    #   card_types = ["monster"])
