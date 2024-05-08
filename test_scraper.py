d44e70ab156c78de023104aec0fa1e483e20371d
Eric Ríos <70504872+EricR401S@users.noreply.github.com>
2024-05-08 21:51:36 +0000

    cleaning up

--- test_scraper.py
+++
@@ -1,7 +0,0 @@
-from scraping_functions.pipeline import scrape_archetypes
-
-
-
-if __name__ == "__main__":
-    archetypes = "Dark Magician,NEos, Blue-Eyes"
-    scrape_archetypes(archetypes, data_path="training_images", csv_path="training_images", csv_name="training_cards.csv")
